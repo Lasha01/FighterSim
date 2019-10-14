8446ff09bf27f2ecd03473f6904894c71cf89764
Lasha01 <55137959+Lasha01@users.noreply.github.com>
2019-10-14 11:04:00 +0200

    Almost the whole game

---
+++ FighterSim/FighterSim/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FighterSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighter f1 = new Fighter();
+
+            Fighter f2 = new Fighter();
+
+            while (f1.IsAlive() && f2.IsAlive())
+            {
+                int dmg1 = f1.Attack();
+
+                f2.Hurt(dmg1);
+
+                int dmg2 = f2.Attack();
+
+                f1.Hurt(dmg2);
+
+                Console.WriteLine( f2.name + "'s hp is " + f2.GetHp());
+                Console.WriteLine( f1.name + "'s hp is " + f1.GetHp());
+
+                Console.WriteLine("Press any button to continue...");
+                Console.ReadLine();
+
+                Console.Clear();
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}

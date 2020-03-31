39cf59d5d384cfb5de6f9cab32623b842d676a65
Lasha01 <55137959+Lasha01@users.noreply.github.com>
2020-03-31 12:45:20 +0200

    Update Program.cs

--- FighterSim/FighterSim/Program.cs
+++ FighterSim/FighterSim/Program.cs
@@ -8,9 +8,9 @@ namespace FighterSim
     class Program
     {
         public static List<Pokemon> pokemons = new List<Pokemon>();  //Skapar en lista med alla pokemons
-        static Random rng = new Random();
+       
 
-        static void Start()  //Alla pokemons skapas som arv med deras egna hp från API: och läggs in i listan med pokemons
+        static void Apicalls()  //Alla pokemons skapas som arv med deras egna hp från API: och läggs in i listan med pokemons
         {
             RestClient client = new RestClient("https://pokeapi.co/api/v2/");
             RestRequest request = new RestRequest("pokemon/magikarp/");
@@ -47,44 +47,27 @@ namespace FighterSim
             totodile.UpdateStats();
             pokemons.Add(totodile);
         }
-
-        private static void Main(string[] args)
+        static void Start()
         {
-            Start();
             Console.WriteLine("PokeKombat Simulator 4");
             Console.ReadLine();
             Console.Clear();
+        }
+        private static void Main(string[] args)
+        {
+            Apicalls();
 
-            int randomPokemon = rng.Next(pokemons.Count);
-            int nextPokemon = rng.Next(pokemons.Count);
+            Start();
 
-            Console.CursorVisible = false;
+            Pokemon p1, p2;
+            ChoosePokemons(out p1, out p2);
 
-            int pokemon1 = ChoosePokemon("Who do you want to play as? \nChoose between: "); 
-            Pokemon p1 = pokemons[pokemon1]; //Den pokemon man väljer från listan läggs in i pi
-            Console.Clear();
+            Battle(p1, p2);
 
-            int pokemon2;
+        }
 
-            while (true)
-            {
-                pokemon2 = ChoosePokemon("Who do you want to play against? \nChoose between: ");
-
-                if (pokemon2 == pokemon1) //Ifall man väljer att möta samma pokemon som man själv har valt så måste man välja en ny
-                {
-                    Console.WriteLine("Choose again!\nPress 'any key'!");
-                    Console.ReadKey();
-
-                    Console.Clear();
-                }
-                else break;
-            }
-            Console.Clear();
-            Pokemon p2 = pokemons[pokemon2]; //Den poekmon man väljer att möta läggs in i p2
-
-            Console.CursorVisible = true;
-            
-
+        private static void Battle(Pokemon p1, Pokemon p2)
+        {
             while (p1.IsAlive() && p2.IsAlive()) //Körs så länge båda pokemonen är vid liv.
             {
 
@@ -115,45 +98,72 @@ namespace FighterSim
                 Console.ReadKey();
                 Console.Clear();
             }
+        }
 
-            int ChoosePokemon(string message)
+        static int ChoosePokemon(string message)
+        {
+            int choice = 1;
+            while (true)
             {
-                int choice = 1;
-                while (true)
+                Console.WriteLine(message);
+                for (int i = 0; i < pokemons.Count; i++) //Körs så många gånger som pokemons finns
                 {
-                    Console.WriteLine(message);
-                    for (int i = 0; i < pokemons.Count; i++) //Körs så många gånger som pokemons finns
+                    Console.WriteLine(" " + pokemons[i].SetName()); //Skriver ut listorna
+                }
+                for (int i = 0; i < pokemons.Count; i++)
+                {
+                    if (choice == (1 + i))
                     {
-                        Console.WriteLine(" " + pokemons[i].SetName()); //Skriver ut listorna
+                        Console.SetCursorPosition(0, i + 2); //Sätter ut pilen 
                     }
-                    for (int i = 0; i < pokemons.Count; i++)
+                }
+                Console.Write(">");
+                Console.SetCursorPosition(0, 14);
+                ConsoleKeyInfo keyInfo = Console.ReadKey(); ;
+                if (keyInfo.Key == ConsoleKey.W || keyInfo.Key == ConsoleKey.UpArrow) { choice--; } //Rör pilen upp och ned
+                if (keyInfo.Key == ConsoleKey.S || keyInfo.Key == ConsoleKey.DownArrow) { choice++; }
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    for (int i = 0; i < pokemons.Count; i++) //Man väljer karaktär med pilen
                     {
-                        if (choice == (1 + i))
+                        if (choice == 1 + i)
                         {
-                            Console.SetCursorPosition(0, i + 2); //Sätter ut pilen 
+                            return choice - 1;
                         }
                     }
-                    Console.Write(">");
-                    Console.SetCursorPosition(0, 14);
-                    ConsoleKeyInfo keyInfo = Console.ReadKey(); ;
-                    if (keyInfo.Key == ConsoleKey.W || keyInfo.Key == ConsoleKey.UpArrow) { choice--; } //Rör pilen upp och ned
-                    if (keyInfo.Key == ConsoleKey.S || keyInfo.Key == ConsoleKey.DownArrow) { choice++; }
-                    if (keyInfo.Key == ConsoleKey.Enter)
-                    {
-                        for (int i = 0; i < pokemons.Count; i++) //Man väljer karaktär med pilen
-                        {
-                            if (choice == 1 + i)
-                            {
-                                return choice - 1;
-                            }
-                        }
-                    }
+                }
 
-                    if (choice < 1) { choice = pokemons.Count; } // Gör så att man kan bläddra mella karaktären från olika håll, både uppifrån och nedifrån
-                    if (choice > pokemons.Count) { choice = 1; }
+                if (choice < 1) { choice = pokemons.Count; } // Gör så att man kan bläddra mella karaktären från olika håll, både uppifrån och nedifrån
+                if (choice > pokemons.Count) { choice = 1; }
+                Console.Clear();
+            }
+        }
+        private static void ChoosePokemons(out Pokemon p1, out Pokemon p2)
+        {
+            Console.CursorVisible = false;
+
+            int pokemon1 = ChoosePokemon("Who do you want to play as? \nChoose between: ");
+            p1 = pokemons[pokemon1];
+            Console.Clear();
+
+            int pokemon2;
+
+            while (true)
+            {
+                pokemon2 = ChoosePokemon("Who do you want to play against? \nChoose between: ");
+
+                if (pokemon2 == pokemon1) //Ifall man väljer att möta samma pokemon som man själv har valt så måste man välja en ny
+                {
+                    Console.WriteLine("Choose again!\nPress 'any key'!");
+                    Console.ReadKey();
+
                     Console.Clear();
                 }
+                else break;
             }
+            Console.Clear();
+            p2 = pokemons[pokemon2];
+            Console.CursorVisible = true;
         }
     }
 }
